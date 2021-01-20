3f81d2cfaacad1c48e4fc26e0919f4255d9d695c
dankevich.te <Timofey.Dankevich@gmail.com>
2021-01-20 12:20:05 +0200

    a little bit update

--- splitStrings.cpp
+++ splitStrings.cpp
@@ -12,7 +12,7 @@ vector<string> solution(const string &s) {
         for (int i = 0; i < result.size(); i++) {
                 part += result[i];
                 if ((i + 1) % 2 == 0) {
-                        res.push_back(part);;
+                        res.push_back(part);
                         part = "";
                 }
         }
